584430791c6fa2a988152243ad3dd8a72c40dfc3
Sai Chandu Naru <saichandunaru@gmail.com>
2022-08-23 23:27:21 -0400

    Reworked Rock-Paper-Scissors with UI

--- logic.js
+++ logic.js
@@ -1,4 +1,42 @@
 
+const container = document.querySelectorAll('button');
+
+container.forEach(btn => btn.addEventListener('click',nextStep));
+let count = 0; let player1 = 0; let computerS = 0;
+
+function nextStep(e)
+{
+    const divResults = document.querySelector('.results');
+    const div1 = document.createElement('div');
+    divResults.appendChild(div1);
+
+    count++;
+    if(count == 6)
+    {
+        if(player1 > computerS)
+            div1.textContent = "You win the game!";
+        else if(computerS > player1)
+            div1.textContent = "You lose the game!";
+        else
+            div1.textContent = "The game is tied!";
+        count = 0; player1 = 0; computerS = 0;
+    }
+    else {
+        if(this.textContent == "Rock!")
+            div1.textContent = playRound("Rock",getComputerChoice());
+        else if(this.textContent == "Paper!")
+            div1.textContent = playRound("Paper",getComputerChoice());
+        else
+            div1.textContent = playRound("Scissors",getComputerChoice());
+
+        if(div1.textContent[4] == 'W')
+            player1++;
+        else if(div1.textContent[4] == 'L')
+            computerS++;
+    }
+};
+
+
 function getComputerChoice() {
     let randVal = Math.floor(Math.random() * 3);
         
@@ -26,9 +64,18 @@ function playRound(playerSelection,computerSelection)
         return "You Lose! Scissors beats Paper";
     else if(playerSelection == "Scissors" && computerSelection == "Paper")
         return "You Win! Scissors beats Paper";
+    else
+        return "boohooo";
 }
 
+
+
+
+
+
+/*
 let player1Score = 0; let computerScore = 0;
+
 for(let i = 0;i < 5;i++)
 {
     playerSelection = prompt("Type one of the following choices: 'Rock','Paper','Scissors'");
@@ -41,7 +88,10 @@ for(let i = 0;i < 5;i++)
         computerScore++;
 }
 
+
 if(player1Score > computerScore)
     console.log("You Won the Game!");
 else
     console.log("You lost the Game!");
+
+*/
